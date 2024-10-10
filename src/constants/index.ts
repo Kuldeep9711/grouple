9cd7100cde264bb163904947cab43462ec2450a0
Delta student <k72125642@gmail.com>
2024-10-10 19:47:45 +0530

    feat(landing): Initial repository - added navbar - Initial basic boilerplate commit

---
+++ src/constants/index.ts
@@ -0,0 +1,17 @@
+import { LAND_PAGE_MENU, MenuProps } from "./menus";
+
+type GroupleConstantsProps = {
+    landingPageMenu: MenuProps[]
+      //  signUpForm: AuthFormProps[]
+      //  signINForm: AuthFormProps[]
+      //  groupList: GroupListProps[]
+       // createGroupPlaceholder: CreateGroupPlaceholderProps[]
+}
+
+export const GROUPLE_CONSTANT:GroupleConstantsProps = {
+ landingPageMenu: LAND_PAGE_MENU,
+   // signUpForm:SIGN_UP_FORM,
+   // signInForm:SIGN_IN_FORM,
+   // groupList:GROUP_LIST,
+   // createGroupPlaceholder:CREATE_GROUP_PLACEHOLDER,
+}
